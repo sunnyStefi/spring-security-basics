3c4dd3a3b729ad97fa298acce30daf135b21479f
stefania <stefania.pozzi.it@gmail.com>
2025-05-05 09:48:42 +0200

    versioning

---
+++ src/test/kotlin/org/security/basics/BasicsApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.security.basics
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BasicsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
