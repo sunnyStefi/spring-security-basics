112fc6658b36a7e87a1ba0a7f9551a8365aa4eb1
stefania <stefania.pozzi.it@gmail.com>
2025-05-05 09:48:42 +0200

    added demo makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+demo:
+	curl -vu user:ADD_FROM_CONSOLE_OUTPUT  http://localhost:8080/hello
---
+++ src/main/kotlin/org/security/basics/HelloController.kt
@@ -0,0 +1,13 @@
+package org.security.basics
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello Mama"
+    }
+}
