05d949342818f6ebd939fdb237040e290f1c5353
stefania <stefania.pozzi.it@gmail.com>
2025-05-19 16:32:01 +0200

    print applied filters

--- src/main/kotlin/org/security/basics/BasicsConfig.kt
+++ src/main/kotlin/org/security/basics/BasicsConfig.kt
@@ -1,14 +1,16 @@
 package org.security.basics
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.FilterChainProxy
 import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
-class BasicsConfig() {
+class BasicsConfig {
 
     @Bean
     fun configure(http: HttpSecurity): SecurityFilterChain {
@@ -17,4 +19,17 @@ class BasicsConfig() {
         return http.build()
     }
 
+    @Bean
+    fun logFilters(filterChainProxy: FilterChainProxy): CommandLineRunner {
+        return CommandLineRunner {
+            println("=== Spring Security Filter Chain ===")
+            filterChainProxy.filterChains.forEach { chain ->
+                println("Pattern: $chain")
+                chain.filters.forEach { filter ->
+                    println(" - ${filter::class.qualifiedName}")
+                }
+            }
+            println("=====================================")
+        }
+    }
 }
