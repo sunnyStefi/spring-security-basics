ee9b055524f5c40fb859b2defec74dc9d1e65ffc
stefania <stefania.pozzi.it@gmail.com>
2025-06-09 11:40:01 +0200

    added enableWebSecurity annotation, removed https config

--- src/main/kotlin/org/security/basics/BasicsConfig.kt
+++ src/main/kotlin/org/security/basics/BasicsConfig.kt
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.FilterChainProxy
 import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
+@EnableWebSecurity(debug = true)
 class BasicsConfig {
 
     @Bean
